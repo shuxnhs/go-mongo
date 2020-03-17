16b18e4715269006a7e6b247e7872591c8bd1706
hexiaohong <610087273@qq.com>
2020-03-17 13:15:02 +0800

    info信息

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,8 @@
 // @APIVersion 1.0.0
-// @Title beego Test API
-// @Description beego has a very cool tools to autogenerate documents for your API
-// @Contact astaxie@gmail.com
+// @Title Go-Mongo
+// @Description go的mongodb中间件，为项目提供mongodb的API
+// @Contact 610087273@qq.com
 // @TermsOfServiceUrl http://beego.me/
-// @License Apache 2.0
-// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
 package routers
 
 import (
